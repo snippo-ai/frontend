964bc84e08bf9a62c21c0d052a8883f5f57ff445
Manav Kaushal <manavkaushal756@gmail.com>
2025-06-15 16:32:53 +0530

    updated login form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-md flex-col gap-6">
-        <Logo center />
+        <Logo iconSize={32} textClassName="text-xl" center />
         {children}
       </div>
     </div>
