34e1a6d203406f486b9741f8693e8bfedcc7b621
Manav Kaushal <manavkaushal756@gmail.com>
2025-06-16 22:50:12 +0530

    updated login page

--- app/(auth)/login/_components/login-form.tsx
+++ app/(auth)/login/_components/login-form.tsx
@@ -11,6 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
+import { GithubIcon } from "lucide-react";
 import Link from "next/link";
 
 type LoginFormProps = React.ComponentProps<"div"> & { redirectTo?: string };
@@ -20,23 +21,18 @@ const LoginForm: React.FC<LoginFormProps> = ({ className = "", ...props }) => {
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardTitle className="text-fluid-lg">Welcome back</CardTitle>
           <CardDescription>Login with your account to continue</CardDescription>
         </CardHeader>
-        <CardContent>
+        <CardContent> 
           <form autoComplete="off">
-            <div className="grid gap-6">
-              <div className="flex flex-col gap-4">
-                <Button variant="outline" className="w-full">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  Apple
+            <div className="grid gap-6 max-w-full">
+              <div className="flex items-center justify-center w-full gap-4">
+                <Button variant="outline">
+                  <GithubIcon className="size-5" />
+                  Github
                 </Button>
-                <Button variant="outline" className="w-full">
+                <Button variant="outline">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -46,7 +42,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ className = "", ...props }) => {
                   Google
                 </Button>
               </div>
-              <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+              <div className="after:border-border relative text-center text-fluid-xs after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                 <span className="bg-card text-muted-foreground relative z-10 px-2">
                   Or continue with
                 </span>
@@ -58,19 +54,19 @@ const LoginForm: React.FC<LoginFormProps> = ({ className = "", ...props }) => {
                     id="email"
                     name="email"
                     type="email"
-                    placeholder="m@example.com"
+                    placeholder="johndoe@example.com"
                     required
                   />
                 </div>
                 <div className="grid gap-3">
                   <div className="flex items-center">
                     <Label htmlFor="password">Password</Label>
-                    <a
-                      href="#"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
+                    <Link
+                      href="/forget-password"
+                      className="ml-auto text-fluid-xs underline-offset-4 hover:underline"
                     >
                       Forgot your password?
-                    </a>
+                    </Link>
                   </div>
                   <Input
                     id="password"
@@ -83,7 +79,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ className = "", ...props }) => {
                   Login
                 </Button>
               </div>
-              <div className="text-center text-sm">
+              <div className="text-center text-fluid-xs">
                 Don&apos;t have an account?{" "}
                 <Link
                   href="/sign-up"
@@ -96,9 +92,10 @@ const LoginForm: React.FC<LoginFormProps> = ({ className = "", ...props }) => {
           </form>
         </CardContent>
       </Card>
-      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-fluid-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our{" "}
+        <Link href="/terms-of-service">Terms of Service</Link> and{" "}
+        <Link href="/privacy-policy">Privacy Policy</Link>.
       </div>
     </div>
   );
