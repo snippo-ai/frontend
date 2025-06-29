1b35a406007f562fe48af6eb66a0d23e8ecc2e7c
Manav Kaushal <manavkaushal756@gmail.com>
2025-06-29 08:20:40 +0530

    updated theme and layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -31,7 +31,7 @@ const DashboardLayout = async ({ children }: DashboardLayoutProps) => {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 bg-background">
+        <header className="flex h-16 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator
@@ -53,7 +53,7 @@ const DashboardLayout = async ({ children }: DashboardLayoutProps) => {
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0 bg-background">
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           {children}
         </div>
       </SidebarInset>
